e0fcdb5a71ff9aac05ed3f00f3f25be6f59e7872
chhagundeep <116256920+chhagundeep@users.noreply.github.com>
2022-11-03 22:47:41 +0530

    Update prog7.c

--- 2221033/prog7.c
+++ 2221033/prog7.c
@@ -1,18 +1,20 @@
-## Write a program to demonstrate for loop in C
+## Program 7: Write a program to demonstrate for loop in C
 ```
 #include <stdio.h>
-int main(){
-int n;
-printf("Enter number to count to: ");
-scanf("%d",&n);
-for(int i=0; i<n; i++){
-printf("%d\t",i+1);
+int main() 
+{
+int i;
+for (i = 0; i < 5; i++) 
+{
+printf("%d\n", i);
 }
-printf("\n");
 return 0;
 }
 ```
-**OUTPUT=**
-Enter number to count to: 10
-```
-1	2	3	4	5	6	7	8	9	10
+**Output:
+0
+1
+2
+3
+4**
+
